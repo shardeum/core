a900da35f9352cfafffbef8047272803d1459dd2
Chris Chabot <chris.chabot@shardeum.org>
2025-03-18 09:28:22 +0000

    Error: Cannot find module '../../src/config/localConfig' has blocked progress on the GCP project for multiple days. This is a temporary workaround so it can be looked at without blocking

--- src/p2p/Self.ts
+++ src/p2p/Self.ts
@@ -36,8 +36,19 @@ import * as ServiceQueue from './ServiceQueue'
 import Shardus from '../shardus'
 import path from 'path'
 import fs from 'fs/promises'
-import { localConfig } from '../config/localConfig'
 import { sleep } from '../utils'
+
+interface LocalConfig {
+    verticalScalingMode_enabled: boolean
+    verticalScalingMode_path: string
+    verticalScalingMode_shutdown_mode: 'sleep' | 'shutdown'
+}
+
+export const localConfig: LocalConfig = {
+    verticalScalingMode_enabled: false || process.env.VERTICALSCALING_ENABLED === 'true',
+    verticalScalingMode_path: '/home/node/config' || process.env.VERTICALSCALING_PATH,
+    verticalScalingMode_shutdown_mode: process.env.VERTICALSCALING_SHUTDOWN_MODE === 'sleep' ? 'sleep' : 'shutdown'
+}
 
 /** STATE */
 
