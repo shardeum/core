ab9d89913598685b467790eb8a100fb46461ea3e
Zachary Belford <belfordz66@gmail.com>
2025-05-21 22:39:33 -0600

    Integrate seqdiagram logging flag

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -364,12 +364,14 @@ class Logger {
     logFlags.fatal = true
     logFlags.important_as_fatal = true
     logFlags.playback = false
+    logFlags.seqdiagram = true
   }
 
   setDisableAllFlags() {
     for (const [key, value] of Object.entries(logFlags)) {
       logFlags[key] = false
     }
+    logFlags.seqdiagram = true
   }
 
   setErrorFlags() {
@@ -382,6 +384,7 @@ class Logger {
     logFlags.important_as_error = true
 
     logFlags.playback = false
+    logFlags.seqdiagram = true
 
     //temp debug
     // logFlags.aalg = true
@@ -403,6 +406,8 @@ class Logger {
 
     logFlags.important_as_fatal = true
     logFlags.important_as_error = true
+
+    logFlags.seqdiagram = true
 
     //logFlags.rotation = true
   }
