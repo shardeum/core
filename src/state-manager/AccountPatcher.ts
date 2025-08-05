501a355249311037068e62950455e2dc53394ece
andrew <44451818+afostr@users.noreply.github.com>
2025-08-05 08:40:17 -0500

    patcher crash fix

--- src/state-manager/AccountPatcher.ts
+++ src/state-manager/AccountPatcher.ts
@@ -2775,7 +2775,7 @@ class AccountPatcher {
 
       // for non-wrapped ranges
       if (startRadix <= endRadix) {
-        for (let i = 0; i <= isInsyncResult.radixes.length; i++) {
+        for (let i = 0; i < isInsyncResult.radixes.length; i++) {
           const radixEntry = isInsyncResult.radixes[i]
           if (radixEntry.radix >= startRadix && radixEntry.radix <= endRadix) {
             radixEntry.recentRuntimeSync = true
@@ -2784,7 +2784,7 @@ class AccountPatcher {
         }
         // for wrapped ranges because we start at the end and wrap around to the beginning of 32 byte address space
       } else {
-        for (let i = 0; i <= isInsyncResult.radixes.length; i++) {
+        for (let i = 0; i < isInsyncResult.radixes.length; i++) {
           const radixEntry = isInsyncResult.radixes[i]
           if (radixEntry.radix >= startRadix || radixEntry.radix <= endRadix) {
             radixEntry.recentRuntimeSync = true
