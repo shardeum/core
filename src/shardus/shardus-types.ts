57bf7302a061d6855ac67a23098f5ffae448dd87
tim <tim@shardeum.org>
2025-08-19 23:02:39 -0300

    added timestamp to out
    Signed-off-by: tim <tim@shardeum.org>

--- src/shardus/shardus-types.ts
+++ src/shardus/shardus-types.ts
@@ -1460,7 +1460,7 @@ export interface LogsConfiguration {
     appenders?: {
       out?: {
         type?: string
-        layout?: { type:string }
+        layout?: { type: string; pattern?: string; tokens?: { [name: string]: any } }
         maxLogSize?: number
         backups?: number
       }
